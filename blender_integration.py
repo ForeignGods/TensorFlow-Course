27e975d3e33ab8bc95579c19ea5e5211e75a314e
ForeignGods <pascal.roessler@gmx.ch>
2022-07-20 19:07:53 +0200

    Update blender_integration.py

--- blender_integration.py
+++ blender_integration.py
@@ -32,6 +32,9 @@ def reset_data():
 
     for cur in bpy.data.curves:
         bpy.data.curves.remove(cur)
+        
+    for collection in bpy.data.collections:
+        bpy.data.collections.remove(collection)
 
 ############################################################
 # Training
@@ -39,7 +42,7 @@ def reset_data():
         
 def training():
 
-    dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
+    dataset, metadata = tfds.load('mnist', as_supervised=True, with_info=True)
     train_dataset, test_dataset = dataset['train'], dataset['test']
 
     class_names = metadata.features['label'].names
@@ -87,9 +90,11 @@ def training():
         train_label = train_label.numpy()
         train_images.append(train_image)
         train_labels.append(train_label)
+    for img in train_images:
+        print(img)
+    for img in train_labels:
+        print(img)
 
-    print(train_images)
-    print(train_labels)
 
     model.fit(train_dataset, epochs=1000, callbacks=[prediction_mc], steps_per_epoch=1)
 
@@ -107,7 +112,7 @@ def training():
             v: reader.get_tensor(v) for v in reader.get_variable_to_shape_map()
         }
         weight_784_25_list.append(state_dict['layer_with_weights-0/kernel/.OPTIMIZER_SLOT/optimizer/v/.ATTRIBUTES/VARIABLE_VALUE'])
-        weight_25_10_list.append(state_dict['layer_with_weights-1/kernel/.OPTIMIZER_SLOT/optimizer/m/.ATTRIBUTES/VARIABLE_VALUE'])
+        weight_25_10_list.append(state_dict['layer_with_weights-1/kernel/.OPTIMIZER_SLOT/optimizer/v/.ATTRIBUTES/VARIABLE_VALUE'])
 
     return weight_784_25_list, weight_25_10_list, train_images, train_labels
    
@@ -151,7 +156,7 @@ def create_nodes():
     return input_list_obj, hidden_list_obj, output_list_obj, input_list_location, hidden_list_location, output_list_location
  
 ############################################################
-# Create Multiple Connections
+# Create Multiple Single Connections
 ############################################################
 
 def create_connection_single(start, end, name):
@@ -172,8 +177,9 @@ def create_connection_single(start, end, name):
     curveOB = bpy.data.objects.new(name, curveData)
 
     # attach to scene and validate context
-    scn = bpy.context.scene
-    bpy.context.collection.objects.link(curveOB)
+    #scn = bpy.context.scene
+    #bpy.context.collection.objects.link(curveOB)
+    
     return curveOB
 
 ############################################################
@@ -221,11 +227,15 @@ def create_connection_complex(start_location_list, end_location_list, name, star
 ############################################################
 
 def single_assign_weight(name, start_location_list, end_location_list, weight_value_list, start_size, end_size):
+    collection = bpy.context.blend_data.collections.new(name=name)
+    bpy.context.collection.children.link(collection)
     for x in range(start_size):
         max_index = np.argmax(weight_value_list[x])
         for y in range(end_size):
-            if(weight_value_list[x][max_index] == weight_value_list[x][y]):
-                curveOB = create_connection_single(start_location_list[x], end_location_list[y],"input_hidden")
+            #print(weight_value_list[x][max_index]," == ",weight_value_list[x][y])
+            if(weight_value_list[x][max_index] == weight_value_list[x][y] and weight_value_list[x][y] != 0 ):
+                #print("connections created")
+                curveOB = create_connection_single(start_location_list[x], end_location_list[y], name)
                 material = bpy.data.materials.new(name = name)
                 material.use_nodes = True
                 bpy.context.object.active_material = material
@@ -233,7 +243,44 @@ def single_assign_weight(name, start_location_list, end_location_list, weight_va
                 principled_node.inputs[19].default_value = (1,1,1,1)
                 principled_node.inputs[20].default_value = 10
                 curveOB.data.materials.append(material)
+                collection.objects.link(curveOB)
+                
+############################################################
+# Assign 28x28 materials Input
+############################################################
 
+def numpy_array_to_material(obj_list, labels_features, name):
+    materials = []
+    i=0
+    if(name=="input"):
+        pixels = labels_features.reshape(-1)
+        for obj in obj_list:
+            material = bpy.data.materials.new(name = name)
+            material.use_nodes = True
+            bpy.context.object.active_material = material
+            principled_node = material.node_tree.nodes.get("Principled BSDF")
+            principled_node.inputs[19].default_value = (1,1,1,1)
+            principled_node.inputs[20].default_value = pixels[i]
+            obj.data.materials.append(material)
+            i += 1
+    else:
+        pixels = labels_features
+        for obj in obj_list:
+            if(labels_features == i):
+                color = 1
+            else:
+                color = 0
+            material = bpy.data.materials.new(name = name)
+            material.use_nodes = True
+            bpy.context.object.active_material = material
+            principled_node = material.node_tree.nodes.get("Principled BSDF")
+            principled_node.inputs[19].default_value = (1,1,1,1)
+            principled_node.inputs[20].default_value = color
+            obj.data.materials.append(material)
+            i += 1
+                
+    return materials
+                             
 ############################################################
 # Main
 ############################################################
@@ -245,10 +292,36 @@ weight_784_25_list, weight_25_10_list, train_images, train_labels = training()
 
 input_list_obj, hidden_list_obj, output_list_obj, input_list_location, hidden_list_location, output_list_location = create_nodes()
 
-single_assign_weight("input_hidden", input_list_location, hidden_list_location, weight_784_25_list, 784, 25)
+material_list_input_hidden = single_assign_weight("input_hidden", input_list_location, hidden_list_location, weight_784_25_list[0], 784, 25)
 
-single_assign_weight("hidden_output", input_list_location, hidden_list_location, weight_25_10_list, 25, 10)
+material_list_hidden_output = single_assign_weight("hidden_output", hidden_list_location, output_list_location, weight_25_10_list[0], 25, 10)
 
 create_connection_complex(input_list_location, hidden_list_location, "784_25_complex", 784, 25)
 
 create_connection_complex(hidden_list_location, output_list_location, "25_10_complex", 25, 10)
+
+input_materials = numpy_array_to_material(input_list_obj, train_images[6], "input")
+
+output_materials = numpy_array_to_material(output_list_obj, train_labels[6], "output")
+
+#def turn_on(view_layer: bpy.types.ViewLayer, collection_include: bpy.types.Collection):
+#    for layer_collection in view_layer.layer_collection.children['ScriptAnimated'].children:
+#        if layer_collection.collection == collection_include:
+#            layer_collection.exclude = False
+# 
+#def turn_off(view_layer: bpy.types.ViewLayer, collection_include: bpy.types.Collection):
+#    for layer_collection in view_layer.layer_collection.children['ScriptAnimated'].children:
+#        if layer_collection.collection == collection_include:
+#            layer_collection.exclude = True
+# 
+#def every_frame(scene):
+#    print("Frame Change", scene.frame_current)
+#    view_layer = bpy.context.scene.view_layers['View Layer']
+#    
+# #collection exclude animation
+#    if scene.frame_current == 700:
+#        turn_off(view_layer, bpy.data.collections["test"])
+#    elif scene.frame_current == 1001: 
+#        turn_on(view_layer, bpy.data.collections["test"])
+# 
+#bpy.app.handlers.frame_change_pre.append(every_frame)
